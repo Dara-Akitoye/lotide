bcbadf8db1b66bdcc7c6132b5d36e2260c6f81a6
Dara Akitoye <daraakitoye1@gmail.com>
2021-07-26 02:54:40 +0000

    implement asserArrayEquals

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = function (arrOne, arrTwo) {
+  let ans = arrOne.length === arrTwo.length &&
+    arrOne.every(function (element, index) {
+      return element === arrTwo[index];
+    })
+  return ans;
+}
+
+const assertArrayEqual = function (arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo) === true) {
+    return `✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`
+  } else {
+    return `🛑🛑🛑 Assertion Failes: ${arrOne} !== ${arrTwo}`
+  }
+}
+
+console.log(assertArrayEqual([1, 2, 3], [1, 2, 3]));
