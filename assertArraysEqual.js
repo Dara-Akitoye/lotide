2a1e55b288b714abda6ccd6ca4df68a42d0ab774
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-12 08:54:00 +0000

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,11 @@
-const eqArrays = function (arrOne, arrTwo) {
-  let ans = arrOne.length === arrTwo.length &&
-    arrOne.every(function (element, index) {
-      return element === arrTwo[index];
-    })
-  return ans;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArrayEqual = function (arrOne, arrTwo) {
   if (eqArrays(arrOne, arrTwo) === true) {
-    return `✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`
+    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
   } else {
-    return `🛑🛑🛑 Assertion Failes: ${arrOne} !== ${arrTwo}`
+    console.log(`🛑🛑🛑 Assertion Failes: ${arrOne} !== ${arrTwo}`)
   }
 }
 
-console.log(assertArrayEqual([1, 2, 3], [1, 2, 3]));
+module.exports = assertArrayEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,2 @@
+const assertArrayEqual = require('../assertArraysEqual');
+assertArrayEqual([1, 2, 3], [1, 2, 3]);
