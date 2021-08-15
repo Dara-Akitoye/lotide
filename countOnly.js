083a703efc3864e12cacffd99a2f94fc4733bbd5
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-15 00:06:33 +0000

    testing commit message

--- countOnly.js
+++ countOnly.js
@@ -6,8 +6,7 @@ const assertEqual = function (actual, expected) {
   }
 }
 
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
+
 const countOnly = function (allItems, itemsToCount) {
   const results = {}
   for (const item of allItems) {
