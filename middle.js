1c6ac76599139f18dbd3d103079b326cfd552632
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-15 01:05:32 +0000

    fixed some bugs in regards to new test code

--- middle.js
+++ middle.js
@@ -2,11 +2,16 @@ const assertArrayEqual = require('./assertArraysEqual');
 
 const middle = function (array) {
   const step = Math.floor(array.length / 2)
+  if (array.length <= 2) {
+    return [];
+  }
   for (let i = 0; i < array.length; i++) {
     if (array.length % 2 !== 0) {
       return [array[step]]
     } else if (array.length % 2 === 0) {
       return [array[step - 1], array[step]];
+    } else if (array.length <= 1) {
+      return [];
     }
   }
 }
